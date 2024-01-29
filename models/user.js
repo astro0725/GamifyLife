e0c9a59ff704873df155ce6992226ad38400a99f
astro <angelica.strong0725@gmail.com>
2024-01-29 11:36:58 -0800

    added email field

--- models/user.js
+++ models/user.js
@@ -7,6 +7,11 @@ module.exports = (sequelize, DataTypes) => {
           unique: true,
           primaryKey: true
       },
+      email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true
+      },
       username: {
           type: DataTypes.STRING,
           allowNull: false,
