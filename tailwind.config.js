fe1313fd1fd375d9c1df442fd0018504cd4925a8
astro <angelica.strong0725@gmail.com>
2024-01-29 14:37:19 -0800

    updated config to handle tailwind in hbs file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./views/partials/*.handlebars", "./views/*.handlebars"],
   theme: {
     extend: {},
   },
