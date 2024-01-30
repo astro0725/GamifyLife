7e16d1ddd70bed140b45fc22e6f3d1260a845ce5
astro <angelica.strong0725@gmail.com>
2024-01-29 17:21:33 -0800

    change to module exports instead of export function

--- utils/authCheck.js
+++ utils/authCheck.js
@@ -1,7 +1,9 @@
-export function checkAuthentication(req, res, next) {
+function checkAuthentication(req, res, next) {
   if (req.session.userId) {
     next(); 
   } else {
     res.render('signup');
   }
 }
+
+module.exports = checkAuthentication;
