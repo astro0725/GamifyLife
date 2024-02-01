b3020c14f26e5463fa458d34235decd7fb590003
astro <angelica.strong0725@gmail.com>
2024-01-31 22:22:52 -0800

    changed 2 async

--- public/js/userAuth/signin.js
+++ public/js/userAuth/signin.js
@@ -1,42 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const signinForm = document.getElementById('signinForm'); 
+document.addEventListener('DOMContentLoaded', () => {
+  const signinForm = document.getElementById('signinForm');
 
   if (signinForm) {
-  console.log("Signin form found.");
+    console.log("Signin form found.");
 
-  signinForm.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+    signinForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const username = document.getElementById('username').value;
 
-    console.log("Email:", email);
-    console.log("Password:", password); 
+      console.log("Email:", email);
+      console.log("Password:", password);
+      console.log("Username:", username);
 
-    fetch('/user/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password}),
-    })
-    .then(response => {
-      console.log("Response received:", response);
+      try {
+        const response = await fetch('/user/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password, username }),
+        });
+
+        console.log("Response received:", response);
 
         if (response.ok) {
-          return response.json();
+          const data = await response.json();
+          console.log("Signin successful:", data);
         } else {
           throw new Error('Signin failed');
         }
-    })
-    .then(data => {
-        console.log("Signin successful:", data);
-    })
-    .catch(error => {
-        console.error("Error during signin:", error);
+      } catch (error) {
+        console.error("Error during signin:", error.message);
+      }
     });
-  });
   } else {
-      console.log("Signin form not found");
+    console.log("Signin form not found");
   }
 });
