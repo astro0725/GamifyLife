1866ec62a6de41c81d4bb47fd6812efad2e0cac3
astro <angelica.strong0725@gmail.com>
2024-02-08 11:40:09 -0800

    test fix for header in deployed site p2

--- public/js/header/signIn.js
+++ public/js/header/signIn.js
@@ -1,4 +1,3 @@
-document.getElementById('signinBtn').addEventListener('click', function(event) {
-  event.preventDefault();
+document.getElementById('signinBtn').addEventListener('click', function() {
   window.location.href = '/user/signin';
 });
