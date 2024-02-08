f785fa08c935b45e5dfaacf6ffce65947987fe05
astro <angelica.strong0725@gmail.com>
2024-02-07 17:22:10 -0800

    fixed dash render

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,14 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { renderDashboard } = require('../controllers/renderDash');
 
-function isAuthenticated(req, res, next) {
-  if (req.session && req.session.userId) {
-      return next();
-  } else {
-      res.redirect('/login');
-  }
-}
-
-router.get('/dashboard', isAuthenticated, renderDashboard);
+router.get('/', renderDashboard);
 
 module.exports = router;
